6bf9c191a6a26d2a7a51359dd8f9bb6fda033553
moohamed154 <medohs770@gmail.com>
2026-06-03 15:52:22 +0300

    feat: add responsive change password view with form validation and state handling

--- lib/features/change_password/view/widgets/change_password_view_body.dart
+++ lib/features/change_password/view/widgets/change_password_view_body.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:safqaseller/core/responsive/breakpoints.dart';
 import 'package:safqaseller/core/utils/app_text_styles.dart';
+import 'package:safqaseller/core/widgets/responsive_form_widgets.dart';
 import 'package:safqaseller/core/widgets/custom_button.dart';
 import 'package:safqaseller/core/widgets/custom_loading_button.dart';
 import 'package:safqaseller/core/widgets/password_field.dart';
@@ -48,51 +50,91 @@ class _ChangePasswordViewBodyState extends State<ChangePasswordViewBody> {
       },
       builder: (context, state) {
         final isLoading = state is ChangePasswordLoading;
+        final isTabletOrUp = Breakpoints.isTabletOrUp(context);
 
         return SafeArea(
           child: SingleChildScrollView(
-            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 24.h),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _FieldLabel(text: S.of(context).kCurrentPassword),
-                  SizedBox(height: 10.h),
-                  PasswordField(
-                    controller: _currentPasswordController,
-                    hintText: '',
-                    enabled: !isLoading,
-                    validator: _validateCurrentPassword,
-                  ),
-                  SizedBox(height: 24.h),
-                  _FieldLabel(text: S.of(context).kNewPassword),
-                  SizedBox(height: 10.h),
-                  PasswordField(
-                    controller: _newPasswordController,
-                    hintText: '',
-                    enabled: !isLoading,
-                    validator: _validateNewPassword,
-                  ),
-                  SizedBox(height: 24.h),
-                  _FieldLabel(text: S.of(context).kConfirmPassword),
-                  SizedBox(height: 10.h),
-                  PasswordField(
-                    controller: _confirmPasswordController,
-                    hintText: '',
-                    enabled: !isLoading,
-                    validator: _validateConfirmPassword,
-                  ),
-                  SizedBox(height: 36.h),
-                  isLoading
-                      ? const CustomLoadingButton()
-                      : CustomButton(
-                          onPressed: _submit,
-                          text: S.of(context).kChangePassword,
-                          textColor: Colors.white,
-                          backgroundColor: Theme.of(context).colorScheme.primary,
+            padding: isTabletOrUp 
+                ? const EdgeInsets.symmetric(vertical: 24.0) 
+                : EdgeInsets.symmetric(horizontal: 20.w, vertical: 24.h),
+            child: ResponsiveFormShell(
+              enabled: isTabletOrUp,
+              maxWidth: 700,
+              child: ResponsiveFormSection(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _FieldLabel(text: S.of(context).kCurrentPassword),
+                      SizedBox(height: isTabletOrUp ? 10.0 : 10.h),
+                      PasswordField(
+                        controller: _currentPasswordController,
+                        hintText: '',
+                        enabled: !isLoading,
+                        validator: _validateCurrentPassword,
+                      ),
+                      SizedBox(height: isTabletOrUp ? 24.0 : 24.h),
+                      if (isTabletOrUp)
+                        ResponsiveFormRow(
+                          leading: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              _FieldLabel(text: S.of(context).kNewPassword),
+                              SizedBox(height: 10.0),
+                              PasswordField(
+                                controller: _newPasswordController,
+                                hintText: '',
+                                enabled: !isLoading,
+                                validator: _validateNewPassword,
+                              ),
+                            ],
+                          ),
+                          trailing: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              _FieldLabel(text: S.of(context).kConfirmPassword),
+                              SizedBox(height: 10.0),
+                              PasswordField(
+                                controller: _confirmPasswordController,
+                                hintText: '',
+                                enabled: !isLoading,
+                                validator: _validateConfirmPassword,
+                              ),
+                            ],
+                          ),
+                        )
+                      else ...[
+                        _FieldLabel(text: S.of(context).kNewPassword),
+                        SizedBox(height: 10.h),
+                        PasswordField(
+                          controller: _newPasswordController,
+                          hintText: '',
+                          enabled: !isLoading,
+                          validator: _validateNewPassword,
                         ),
-                ],
+                        SizedBox(height: 24.h),
+                        _FieldLabel(text: S.of(context).kConfirmPassword),
+                        SizedBox(height: 10.h),
+                        PasswordField(
+                          controller: _confirmPasswordController,
+                          hintText: '',
+                          enabled: !isLoading,
+                          validator: _validateConfirmPassword,
+                        ),
+                      ],
+                      SizedBox(height: isTabletOrUp ? 36.0 : 36.h),
+                      isLoading
+                          ? const CustomLoadingButton()
+                          : CustomButton(
+                              onPressed: _submit,
+                              text: S.of(context).kChangePassword,
+                              textColor: Colors.white,
+                              backgroundColor: Theme.of(context).colorScheme.primary,
+                            ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
